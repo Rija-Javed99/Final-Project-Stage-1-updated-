7787287bc4801ec2d70965426857de38b4a18073
Rija-Javed99 <73756430+Rija-Javed99@users.noreply.github.com>
2021-06-17 00:31:27 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,23 +1,29 @@
-
+var bg, bgimg;
 
 
 function preload(){
-
+bgimg=loadImage("images/bg3.jpg")
   
 }
 
 
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth,windowHeight);
   
+  bg = createSprite(width/2,height/2,width,400);
+  bg.addImage("ground",bgimg);
+  bg.scale= 0.6
+  bg.velocityX = -3 ;
  
 }
 
 function draw() {
  background(0);
   
- 
+ if (bg.x < 0){
+  bg.x = width/2
+}
 
   drawSprites();
  
